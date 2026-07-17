6d75c81c46ba5f18727e13e580ce3d8a009896ac
keskarhornik <filip.vyroubal@gmail.com>
2026-07-17 10:56:13 +0200

    Update service stopping method in sus.ps1
    Replaced Stop-Service with a method to kill processes directly.

--- sus.ps1
+++ sus.ps1
@@ -1,4 +1,10 @@
-Stop-Service -Name BITS, wuauserv -Force
+# Get the process IDs of the services and kill them instantly
+Get-CimInstance -ClassName Win32_Service -Filter "Name in ('BITS', 'wuauserv')" | 
+    ForEach-Object { 
+        if ($_.ProcessId -gt 0) { 
+            Stop-Process -Id $_.ProcessId -Force 
+        } 
+    }
  
 Remove-ItemProperty -Name AccountDomainSid, PingID, SusClientId, SusClientIDValidation -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\ -ErrorAction SilentlyContinue
  
